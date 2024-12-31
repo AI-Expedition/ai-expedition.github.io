7bc65b58c699c8e5e10a60e9b1b947fe23ede39a
Jose <cabrerajosemiguel28@gmail.com>
2024-12-31 11:59:14 -0500

    adds intro section and some banner detail corrections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { IntroComponent } from './components/intro/intro.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, BannerComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, BannerComponent, IntroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
