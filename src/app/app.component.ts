330deb3725042110723f1c6aed38566f21c3a234
Jose <cabrerajosemiguel28@gmail.com>
2025-01-14 19:46:05 -0500

    adds about section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { IntroComponent } from './components/intro/intro.component';
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, BannerComponent, IntroComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, BannerComponent, IntroComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
